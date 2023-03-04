42c02a1f6ade9e4b5cf37d153d7c3e2ac5e70b59
MatthieuAM <79106996+MatthieuAM@users.noreply.github.com>
2023-03-04 19:32:34 +0100

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,17 @@
+
+/*var validation = document.getElementById('bouton_envoi');
+var prenom = document.getElementById('name');
+var prenom_m = document.getElementById('prenom_manquant');
+validation.addEventListener('click', f_valid);
+
+
+function f_valid(e){
+
+    if(prenom.validity.valueMissing) {
+        e.preventDefault();
+        prenom_m.textContent = "Nom et Prénom manquant ";
+        prenom_m.style.color = 'red';
+    }
+
+
+}*/
---
+++ incluredanspage.js
@@ -0,0 +1,23 @@
+$(function(){
+    $("#inclureNavBar").load("/navbar.html"); 
+  });
+
+
+
+  $(function(){
+    $("#inclureFooter").load("/footer.html"); 
+  });
+
+
+  $(function(){
+    $("#inclureFavicon").load("/favicon.html"); 
+  });
+
+
+  // Chaque fonction permet d'integrer une partie du site le footer en bas de chaque page ect...
+  
+
+  
+
+  // LE RETOUR EN HAUT DE PAGE 
+
